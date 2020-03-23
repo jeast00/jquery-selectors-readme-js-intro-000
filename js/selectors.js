6c7c206e68d25579509738dc677ce95fe42ce01f
Jonathan East <jonathan_east@ymail.com>
2020-03-23 02:53:25 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,4 +1,20 @@
 // declare your functions here...
 function paragraphSelector() {
-  $('p');
+
+}
+
+function lastImageSelector() {
+
+}
+
+function ninjaBabySelector() {
+
+}
+
+function divSelector() {
+
+}
+
+function firstListItem() {
+
 }
